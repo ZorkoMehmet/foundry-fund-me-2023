78d2a75fdc6576b0ffc0f8df60422997ffb2259c
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2023-08-12 13:32:17 -0400

    our first commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,64 @@
+//SPDX-License-Identifier: MIT
+
+//1. Deploy mocks when we are on a local anvil chain
+//2. Keep track of contract addresses across different chains
+//Sepholia ETH/USD
+//Mainnet ETH/USD
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script{
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor (){
+        if(block.chainid == 11155111){
+            activeNetworkConfig = getSepholiaEthConfig();
+        } else if(block.chainid == 1){
+            activeNetworkConfig = getMainnetEthConfig();
+        } else{
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+    
+    
+    function getSepholiaEthConfig() public pure returns(NetworkConfig memory){
+
+        NetworkConfig memory sepholiaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepholiaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns(NetworkConfig memory){
+
+        NetworkConfig memory mainnetEthConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return mainnetEthConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory){
+        if(activeNetworkConfig.priceFeed != address(0)){
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)});
+        return anvilConfig;
+    }
+}
